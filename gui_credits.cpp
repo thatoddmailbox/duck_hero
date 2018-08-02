c828fa2896f66d4e5cfe678db4581aae71955cb3
Alex Studer <astuder@notetoscreen.com>
2018-08-02 01:52:29 -0400

    fill in credits

--- gui_credits.cpp
+++ gui_credits.cpp
@@ -14,7 +14,27 @@ namespace duckhero
 		SDL_SetRenderDrawColor(r, 0, 0, 0, 255);
 		SDL_RenderFillRect(r, &window_rect);
 
-		_text_cache.Draw(r, "Duck Hero\n\nwow credits", 10, 10);
+		_text_cache.Draw(r, "Duck Hero\n\
+\n\
+Designed by Ducky Quackers\n\
+Produced by Dogo\n\
+\n\
+Art: Some assets were created by Kenney - http://kenney.nl\n\
+\n\
+Font: Fixedsys Excelsior - http://www.fixedsysexcelsior.com/\n\
+\n\
+Music: Eric Skiff - Resistor Anthems - Available at http://EricSkiff.com/music\n\
+Tracks used: Underclocked, We're the Resistors, HHavok-intro\n\
+\n\
+Open-source software used:\n\
+* SDL - Copyright (C) 1997-2014 Sam Lantinga <slouken@libsdl.org>\n\
+* SDL_gfx - Copyright (C) 2012-2018 A. Schiffler <aschiffler@ferzkopp.net>\n\
+* SDL_image - Copyright (C) 1997-2013 Sam Lantinga <slouken@libsdl.org>\n\
+* SDL_mixer - Copyright (C) 1997-2013 Sam Lantinga <slouken@libsdl.org>\n\
+* SDL_ttf - Copyright (C) 2001-2013 Sam Lantinga <slouken@libsdl.org>\n\
+* PhysicsFS - Copyright (c) 2001-2012 Ryan C. Gordon and others.\n\
+\n\
+Thanks for playing!", 10, 10);
 	}
 
 	void GUICredits::HandleAction()
